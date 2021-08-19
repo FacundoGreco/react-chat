48b7a0ee6db4ed0b030f36462a7137b018665c02
FacundoGreco <facundogrecor@gmail.com>
2021-08-19 19:28:35 -0300

    Fixes messages overflow

--- src/components/MessagesContainer.js
+++ src/components/MessagesContainer.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Message from "./Message";
 import "./MessagesContainer.scss";
 
 export default function MessagesContainer({ messages }) {
+	useEffect(() => {
+		const messagesContainer = document.querySelector(".messagesContainer");
+		messagesContainer.scrollTo(0, messagesContainer.scrollHeight);
+	}, [messages]);
+
 	return (
 		<div className="messagesContainer">
 			{messages && messages.map((message) => <Message message={message} key={message.id} />)}
